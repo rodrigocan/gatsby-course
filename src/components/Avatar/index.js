943da8e948fea3ad924cd458215e684407c71b10
Rodrigo Cândido <rodrigocan@gmail.com>
2021-02-18 14:34:54 -0300

    Understanding fluid and fixed images differences

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -8,8 +8,8 @@ const Avatar = () => {
       query {
         avatarImage: file(relativePath: { eq: "profile-photo.png" }) {
           childImageSharp {
-            fixed(width: 60, height: 60) {
-              ...GatsbyImageSharpFixed
+            fluid(maxWidth: 60, maxHeight: 60) {
+              ...GatsbyImageSharpFluid_tracedSVG
             }
           }
         }
@@ -17,7 +17,9 @@ const Avatar = () => {
     `
   )
 
-  return <Img fixed={avatarImage.childImageSharp.fixed} />
+  return (
+    <Img fluid={avatarImage.childImageSharp.fluid} style={{ width: "60px" }} />
+  )
 }
 
 export default Avatar
