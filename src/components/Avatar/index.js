72560cd58a56d233804b5baa3854c6c31222d3d0
Rodrigo Cândido <rodrigocan@gmail.com>
2021-02-19 00:42:53 -0300

    Creating Avatar Component

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+
+import * as S from "./styled"
 
 const Avatar = () => {
   const { avatarImage } = useStaticQuery(
@@ -8,8 +9,8 @@ const Avatar = () => {
       query {
         avatarImage: file(relativePath: { eq: "profile-photo.png" }) {
           childImageSharp {
-            fluid(maxWidth: 60, maxHeight: 60) {
-              ...GatsbyImageSharpFluid_tracedSVG
+            fixed(width: 60, height: 60) {
+              ...GatsbyImageSharpFixed_tracedSVG
             }
           }
         }
@@ -17,9 +18,7 @@ const Avatar = () => {
     `
   )
 
-  return (
-    <Img fluid={avatarImage.childImageSharp.fluid} style={{ width: "60px" }} />
-  )
+  return <S.AvatarWrapper fixed={avatarImage.childImageSharp.fixed} />
 }
 
 export default Avatar
