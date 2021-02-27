d147db541811b20c6a132b4d68d024977974db31
Rodrigo Candido <rodrigocan@gmail.com>
2021-02-27 16:18:40 -0300

    Creating Sidebar Component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,24 +1,22 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Profile from "../Profile"
+import React from "react";
+import PropTypes from "prop-types";
+import Sidebar from "../Sidebar";
 
-import * as S from "./styled"
-import GlobalStyles from "../../styles/global"
+import * as S from "./styled";
+import GlobalStyles from "../../styles/global";
 
 const Layout = ({ children }) => {
   return (
     <S.LayoutWrapper>
       <GlobalStyles />
-      <aside>
-        <Profile />
-      </aside>
+      <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
     </S.LayoutWrapper>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
