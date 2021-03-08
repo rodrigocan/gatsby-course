8623dfdec148aa0272edbd229e6fa5f4c210b1fa
Rodrigo Candido <rodrigocan@gmail.com>
2021-03-08 17:48:48 -0300

    Integrating layout to blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
 
 const BlogPost = ({ data }) => {
-  const post = data.markdownRemark;
+  const post = data.markdownRemark
 
   return (
-    <>
+    <Layout>
+      <SEO title={post.frontmatter.title} />
       <h1>{post.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
-    </>
-  );
-};
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query Post($slug: String!) {
@@ -21,6 +25,6 @@ export const query = graphql`
       html
     }
   }
-`;
+`
 
-export default BlogPost;
+export default BlogPost
