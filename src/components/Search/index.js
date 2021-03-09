d6b6b222707f0974a3717366d789c7ca89edb977
Rodrigo Candido <rodrigocan@gmail.com>
2021-03-09 20:59:09 -0300

    Config hit results

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -3,6 +3,7 @@ import React from "react"
 import algoliasearch from "algoliasearch/lite"
 import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
 
+import Hit from "./Hit"
 import * as S from "./styled"
 
 const algolia = {
@@ -16,7 +17,7 @@ const searchClient = algoliasearch(algolia.appId, algolia.searchOnlyApiKey)
 const Search = () => (
   <S.SearchWrapper>
     <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
-      <SearchBox autoFocus translations={{ placeholder: "Pesquisar..." }} />
+      <SearchBox translations={{ placeholder: "Pesquisar..." }} />
       <Stats
         translations={{
           stats(nbHits, timeSpentMs) {
@@ -24,7 +25,7 @@ const Search = () => (
           },
         }}
       />
-      <Hits />
+      <Hits hitComponent={Hit} />
     </InstantSearch>
   </S.SearchWrapper>
 )
